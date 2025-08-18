24b1ae2348c18366ae184a1c552520fdf897c9ef
Lukas Wippermann <info@wipmate.de>
2025-08-18 22:51:03 +0200

    simplify dropdown behavior

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -5,22 +5,13 @@ export type DropdownOption = { value: string | number, label: JSX.Element | stri
 
 export function Dropdown({ options, value, setValue, selectDefaultValue = true, displayAsList = false, ref, ...props }: ComponentProps<"div"> & Pick<ComponentProps<"input">, "name" | "ref"> & { options: DropdownOption[], value?: string | number, setValue: (value: string | number) => void, selectDefaultValue?: boolean, displayAsList?: boolean }): JSX.Element {
   const dropdown = useRef<HTMLDivElement>(null);
-  const [inputValue, setInputValue] = useState<string | number | undefined>(value);
   const [expanded, setExpanded] = useState(false);
-  const hasInteracted = useRef(false);
 
   useEffect(() => {
-    if (selectDefaultValue && options.length && !options.map(value => value.value).includes(value as string | number)) {
-      setInputValue(options[0].value);
+    if (selectDefaultValue && options.length && !options.some(option => option.value === value)) {
+      setValue(options[0].value);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps -- dependency to setValue breaks the behavior
-  }, [options, setInputValue]);
-
-  useEffect(() => {
-    if (inputValue !== undefined && (selectDefaultValue || hasInteracted.current)) {
-      setValue(inputValue);
-    }
-  }, [inputValue]);
+  }, [options, setValue]);
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -30,14 +21,14 @@ export function Dropdown({ options, value, setValue, selectDefaultValue = true, 
     };
     document.addEventListener("click", listener);
     return () => document.removeEventListener("click", listener);
-  }, [options]);
+  }, [setExpanded]);
 
   const list = <div className="dropdown-list" style={{ maxHeight: displayAsList ? undefined : 220 }}>
     <div {...(displayAsList ? props : {})}>
       {options.length ? options.map(option => <div
         key={option.value}
         className={"dropdown-item" + (option.value == value ? " active" : "")}
-        onClick={() => (hasInteracted.current = true, setInputValue(option.value))}
+        onClick={() => setValue(option.value)}
       >
         {option.label}
       </div>) : <div className="dropdown-item">No options available</div>}
@@ -45,7 +36,7 @@ export function Dropdown({ options, value, setValue, selectDefaultValue = true, 
   </div>;
 
   return <>
-    <input type="hidden" value={inputValue} name={props.name} ref={ref} />
+    <input type="hidden" value={value} name={props.name} ref={ref} />
     {displayAsList
       ? list
       : <div {...props} className={`dropdown ${expanded ? "expanded" : ""}`} ref={dropdown} onClick={() => dropdown.current && options.length ? setExpanded(pre => !pre) : null}>
